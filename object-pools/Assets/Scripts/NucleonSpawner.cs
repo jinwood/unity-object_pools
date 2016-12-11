a9ad10a2debb860d8b340bba8f70378cd2fc0d6d
Julian Inwood <jinwood8@outlook.com>
2016-12-11 16:39:54 +0000

    Initial commit

---
+++ object-pools/Assets/Scripts/NucleonSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NucleonSpawner : MonoBehaviour {
+	public float timeBetweenSpawns;
+	public float spawnDistance;
+	public Nucleon[] nucleonPrefabs;
+	float timeSinceLastSpawn;
+
+	void FixedUpdate () {
+		timeSinceLastSpawn += Time.deltaTime;
+		if (timeSinceLastSpawn >= timeBetweenSpawns) {
+			timeSinceLastSpawn -= timeBetweenSpawns;
+			SpawnNucleon();
+		}
+	}
+
+	void SpawnNucleon(){
+		var prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+		var spawn = Instantiate<Nucleon>(prefab);
+		spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+	}
+
+	
+}
