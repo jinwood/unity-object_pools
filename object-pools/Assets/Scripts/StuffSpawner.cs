a384a86d426c6a54383748dfd2301a86342ed473
Julian Inwood <jinwood8@outlook.com>
2016-12-11 18:17:40 +0000

    Add spawner ring and spawner pref

---
+++ object-pools/Assets/Scripts/StuffSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StuffSpawner : MonoBehaviour
+{
+
+	public float timeBetweenSpawns;
+	public Stuff[] stuffPrefabs;
+	public float velocity;
+
+	float timeSinceLastSpawn;
+
+	void FixedUpdate ()
+	{
+		timeSinceLastSpawn += Time.deltaTime;
+		if (timeSinceLastSpawn >= timeBetweenSpawns) {
+			timeSinceLastSpawn -= timeBetweenSpawns;
+		}
+		SpawnStuff ();
+	}
+
+	void SpawnStuff ()
+	{
+		var prefab = stuffPrefabs [Random.Range (0, stuffPrefabs.Length)];
+		var spawn = Instantiate<Stuff> (prefab);
+		spawn.transform.localPosition = transform.position;
+		spawn.Body.velocity = transform.up * velocity;
+	}
+}
